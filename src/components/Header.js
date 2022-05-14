8e810c717efa53d6e128eabd2865627a162ed763
Zicheng.S <zichengshan@gmail.com>
2022-05-14 12:07:25 -0700

    Update Header Cart Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-import React from "react"
+import React, {useContext} from "react"
 import {Link} from "react-router-dom";
+import {Context} from "../Context";
+
 
 function Header() {
+    const {cartItems} = useContext(Context)
+
+    const cartClassName = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
     return (
         <header>
             <Link to="/">
                 <h2>Pic Some</h2>
             </Link>
             <Link to="/cart">
-                <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+                <i className={`${cartClassName} ri-fw ri-2x`}></i>
             </Link>
 
         </header>
