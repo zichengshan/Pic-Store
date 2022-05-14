b3596a80e946eb873884e4917c86d64207f2a084
Zicheng.S <zichengshan@gmail.com>
2022-05-14 11:11:49 -0700

    Image propTypes
    1. className should be a string
    2. img should be an object, specifically an object with `id`, `url`, and `isFavorite` properties

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,12 @@
 import React, {useState, useContext} from "react";
+import PropTypes from "prop-types"
 import {Context} from "../Context";
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
     const {toggleFavorite} = useContext(Context)
     const heartIcon = hovered &&
-        (img.isFavorite ? 
+        (img.isFavorite ?
             <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i> :
             <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
         )
@@ -23,6 +24,15 @@ function Image({className, img}) {
             {cartIcon}
         </div>
     )
+}
+
+Image.prototype = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
 }
 
 export default Image
