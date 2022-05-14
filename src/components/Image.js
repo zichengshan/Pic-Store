762a69dfc6fabf0662463690d17cde5a247a5166
Zicheng.S <zichengshan@gmail.com>
2022-05-14 11:03:36 -0700

    Display Favorite

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,12 @@ import {Context} from "../Context";
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
     const {toggleFavorite} = useContext(Context)
-    const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+    const heartIcon = hovered &&
+        (img.isFavorite ? 
+            <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i> :
+            <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+        )
+
     const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
 
     return (
