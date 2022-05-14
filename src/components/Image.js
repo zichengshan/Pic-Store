261a3c592973e6e291fd3ffacc0f6df2259b7b3f
Zicheng.S <zichengshan@gmail.com>
2022-05-14 00:22:11 -0700

    track hover state

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Image({className, img}) {
+    const [hovered, setHovered] = useState(false)
+    
     return (
-        <div className={`${className} image-container`}>
-            <img src={img.url} className="image-grid" />
+        <div
+            className={`${className} image-container`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            <img src={img.url} className="image-grid" alt="pic" />
         </div>
     )
 }
