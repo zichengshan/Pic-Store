696ab8b19c7c57c7e4628e324f7a3d0f468b7ef5
Zicheng.S <zichengshan@gmail.com>
2022-05-13 22:53:02 -0700

    React Router Setup
    1. Click on the words "Pic Some" in the header to go to the "/" route, which should display the Photos component (found in the pages folder)
    
    2. Click on the shopping cart icon in the header to go to the "/cart" route, which should display the Cart component (found in the pages folder)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 
 import React from "react"
+import {Routes, Route} from "react-router-dom";
 import Header from "./components/Header"
+import Photos from "./pages/Photos";
+import Cart from "./pages/Cart";
 
 function App() {
-  return (
-      <div>
-        <Header />
-        <h1>Home Page</h1>
-      </div>
-  )
+    return (
+        <div>
+            <Header/>
+            <Routes>
+                <Route exact path = "/" element = {<Photos />} />
+                <Route exact path = "/cart" element={<Cart />} />
+            </Routes>
+        </div>
+
+    )
 }
 
 export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from "react"
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <header>
-            <h2>Pic Some</h2>
-            <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+            <Link to="/">
+                <h2>Pic Some</h2>
+            </Link>
+            <Link to="/cart">
+                <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+            </Link>
+
         </header>
     )
 }
