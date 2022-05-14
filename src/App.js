4bff7f76da8ddddb7996e747f7e15d1d999143da
Zicheng.S <zichengshan@gmail.com>
2022-05-13 22:27:22 -0700

    Set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from "react"
+import Header from "./components/Header"
+
+function App() {
+  return (
+      <div>
+        <Header />
+        <h1>Home Page</h1>
+      </div>
+  )
+}
+
+export default App
---
+++ src/pages/Photos.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Photos() {
+    return (
+        <main className="photos">
+            <h1>Images go here</h1>
+        </main>
+    )
+}
+
+export default Photos
